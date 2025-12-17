192fbe6218060176579346c3adba2b82b3125f22
Kim Inho <inho524890@gmail.com>
2025-12-18 03:54:31 +0900

    project: update run.sh in attn, conv, moe

--- final-project/run.sh
+++ final-project/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 : ${NODES:=4}
-# : ${PROFILE:=nsys profile --cudabacktrace=all}
 
 salloc -N $NODES --partition class1 --exclusive --gres=gpu:4   \
 	mpirun --bind-to none -mca btl ^openib -npernode 1 \
 		--oversubscribe -quiet \
-		$PROFILE ./main $@
+		./main $@
--- final-project/tests/moe/run.sh
+++ final-project/tests/moe/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-srun --nodes=1 --exclusive --partition=class1 --gres=gpu:1 numactl --physcpubind 0-31 ./main $@
+srun --nodes=1 --exclusive --partition=class1 --gres=gpu:1 numactl --physcpubind 0-31 ./main -v -n 5 16
 # srun --nodes=1 --exclusive --partition=class1 --gres=gpu:1 numactl --physcpubind 0-31 nsys profile --cudabacktrace=all ./main $@
